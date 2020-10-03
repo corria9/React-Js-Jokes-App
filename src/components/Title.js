8370819aa171e1a5e853169773069b8027ac5c5e
Armando Coria <48301256+corria9@users.noreply.github.com>
2020-10-03 00:54:50 -0500

    Add files via upload

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+    return ( 
+        
+        <div>
+            <h3 className="text-center mt-5">Jokes App</h3>
+        </div>
+    
+    );
+}
+ 
+export default Title;
