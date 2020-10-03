a937ea1e30b6fb9d014644a1cc2a96efe21797c7
Armando Coria <48301256+corria9@users.noreply.github.com>
2020-10-03 00:45:44 -0500

    Add files via upload

---
+++ build/precache-manifest.569826e41fd670d654dd8e8fe8594c29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5091fbc2dca5c8d158bb87743928d26b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1ad2af73a5fab61621d",
+    "url": "/static/css/main.c0b63c69.chunk.css"
+  },
+  {
+    "revision": "9047117bab2c944e12cf",
+    "url": "/static/js/2.b768d8f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b768d8f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1ad2af73a5fab61621d",
+    "url": "/static/js/main.92eb1772.chunk.js"
+  },
+  {
+    "revision": "6091c0fbb2b06691a0f8",
+    "url": "/static/js/runtime-main.229ab9e9.js"
+  }
+]);
